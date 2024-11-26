c97c6e5b3bd529777b564008ceb79a6a721d11a3
NathanDSanta <natan.d.santa@gmail.com>
2024-11-26 16:39:54 +0100

    more testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,48 @@
-#include "app-window.h"
-
-int WinMain(int argc, char **argv)
-{
-    auto ui = AppWindow::create();
-
-    ui->on_request_increase_value([&]{
-        ui->set_counter(ui->get_counter() + 1);
-    });
-
-    ui->run();
-    return 0;
+// #include "app-window.h"
+//
+// int WinMain(int argc, char **argv)
+// {
+//     auto ui = AppWindow::create();
+//
+//     ui->on_request_increase_value([&]{
+//         ui->set_counter(ui->get_counter() + 1);
+//     });
+//
+//     ui->run();
+//     return 0;
+// }
+#include <stdio.h>
+#include <sqlite3.h>
+ 
+static int callback(void *NotUsed, int argc, char **argv, char **azColName){
+  int i;
+  for(i=0; i<argc; i++){
+    printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+  }
+  printf("\n");
+  return 0;
 }
-// #include <stdio.h>
-// #include <sqlite3.h>
-//  
-// static int callback(void *NotUsed, int argc, char **argv, char **azColName){
-//   int i;
-//   for(i=0; i<argc; i++){
-//     printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
-//   }
-//   printf("\n");
-//   return 0;
-// }
-//
-// int main(int argc, char **argv){
-//   sqlite3 *db;
-//   char *zErrMsg = 0;
-//   int rc;
-//
-//   if( argc!=3 ){
-//     fprintf(stderr, "Usage: %s DATABASE SQL-STATEMENT\n", argv[0]);
-//     return(1);
-//   }
-//   rc = sqlite3_open(argv[1], &db);
-//   if( rc ){
-//     fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-//     sqlite3_close(db);
-//     return(1);
-//   }
-//   rc = sqlite3_exec(db, argv[2], callback, 0, &zErrMsg);
-//   if( rc!=SQLITE_OK ){
-//     fprintf(stderr, "SQL error: %s\n", zErrMsg);
-//     sqlite3_free(zErrMsg);
-//   }
-//   sqlite3_close(db);
-//   return 0;
-// }
+
+int main(int argc, char **argv){
+  sqlite3 *db;
+  char *zErrMsg = 0;
+  int rc;
+
+  if( argc!=3 ){
+    fprintf(stderr, "Usage: %s DATABASE SQL-STATEMENT\n", argv[0]);
+    return(1);
+  }
+  rc = sqlite3_open(argv[1], &db);
+  if( rc ){
+    fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+    sqlite3_close(db);
+    return(1);
+  }
+  rc = sqlite3_exec(db, argv[2], callback, 0, &zErrMsg);
+  if( rc!=SQLITE_OK ){
+    fprintf(stderr, "SQL error: %s\n", zErrMsg);
+    sqlite3_free(zErrMsg);
+  }
+  sqlite3_close(db);
+  return 0;
+}
