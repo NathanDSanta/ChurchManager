9ec822bc9559245c4df9a973c6a6b4f003f1586d
NathanDSanta <natan.d.santa@gmail.com>
2024-12-13 22:04:35 +0100

    test.db proper structure

---
+++ tests/cppReadTest/insertTestPersons.sql
@@ -0,0 +1,81 @@
+INSERT INTO Person (
+  id,
+  name,
+  surname,
+  gender,
+  birthdate, 
+  address,
+  phone,
+  isMember,
+  memberDate,
+  familyId
+) VALUES ( 
+  '1234',
+  'Viorel',
+  'Santa',
+  'M',
+  '1967-06-19',
+  'Porqueres',
+  627379298,
+  1,
+  '2020-01-01',
+  'SantVio'
+);
+
+INSERT INTO Person (
+  id,
+  name,
+  surname,
+  gender,
+  birthdate, 
+  address,
+  phone,
+  isMember,
+  memberDate,
+  familyId
+) VALUES ( 
+  'X7804455A',
+  'Darius Natan',
+  'Santa',
+  'M',
+  '2005-03-27',
+  'Porqueres',
+  642146353,
+  1,
+  '2024-06-16',
+  'SantVio'
+);
+
+INSERT INTO Person (
+  id,
+  name,
+  surname,
+  gender,
+  birthdate, 
+  address,
+  phone,
+  isMember,
+  memberDate,
+  familyId
+) VALUES ( 
+  'X4906468Q',
+  'Miluta Adriana',
+  'Santa',
+  'F',
+  '1970-02-25',
+  'Porqueres',
+  639272477,
+  1,
+  '2020-01-01',
+  'SantVio'
+);
+
+INSERT into family(
+  id,
+  surname,
+  fatherID
+  ) VALUES (
+  'SantVio',
+  'Santa',
+  '1234'
+  )
